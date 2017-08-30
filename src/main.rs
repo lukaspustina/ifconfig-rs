1849069d566b159d3a1e379abea3aca8c1b9baf4
Lukas Pustina <lukas@pustina.net>
2017-08-30 14:11:22 +0200

    MVP with handlebar templates

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,48 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
+extern crate serde_json;
+#[macro_use] extern crate serde_derive;
+
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{Request, State, Outcome};
+use rocket_contrib::Template;
+use std::net::SocketAddr;
+
+
+struct RequesterInfo {
+    remote: SocketAddr,
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for RequesterInfo {
+    type Error = ();
+
+    fn from_request(req: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        match req.remote() {
+            Some(socket) => Outcome::Success(RequesterInfo{ remote: socket.clone() } ),
+            None => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct TemplateContext {
+    ip: String,
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index(req_info: RequesterInfo) -> Template {
+    let ip = format!("{}", req_info.remote.ip());
+    let context = TemplateContext { ip: ip };
+    Template::render("index", &context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
 
