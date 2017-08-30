5749564303ddab7a0b85f167bfac8a6d9ecb8527
Lukas Pustina <lukas@pustina.net>
2017-08-30 15:41:19 +0200

    Add user-agent to req_info

--- src/main.rs
+++ src/main.rs
@@ -15,28 +15,35 @@ use std::net::SocketAddr;
 
 struct RequesterInfo {
     remote: SocketAddr,
+    user_agent: Option<String>,
 }
 
 impl<'a, 'r> FromRequest<'a, 'r> for RequesterInfo {
     type Error = ();
 
     fn from_request(req: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        match req.remote() {
-            Some(socket) => Outcome::Success(RequesterInfo{ remote: socket.clone() } ),
-            None => Outcome::Failure((Status::ServiceUnavailable, ()))
-        }
+        let remote = if let Some(remote) = req.remote() {
+            remote
+        } else {
+            return Outcome::Failure((Status::BadRequest, ()))
+        };
+        let user_agent = req.headers().get_one("User-Agent").map(|u| u.to_string());
+
+        let request_info = RequesterInfo { remote: remote, user_agent: user_agent };
+        Outcome::Success(request_info)
     }
 }
 
 #[derive(Serialize)]
 struct TemplateContext {
     ip: String,
+    user_agent: Option<String>,
 }
 
 #[get("/")]
 fn index(req_info: RequesterInfo) -> Template {
     let ip = format!("{}", req_info.remote.ip());
-    let context = TemplateContext { ip: ip };
+    let context = TemplateContext { ip: ip, user_agent: req_info.user_agent };
     Template::render("index", &context)
 }
 
