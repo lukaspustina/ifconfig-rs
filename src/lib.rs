2d3f53d3382f532b21d9fe7e748c0def7206e81a
Lukas Pustina <lukas@pustina.net>
2017-08-31 10:32:56 +0200

    Refactore non-rocket code into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+extern crate woothee;
+#[macro_use] extern crate serde_derive;
+
+use std::net::SocketAddr;
+
+pub type UserAgentParser = woothee::parser::Parser;
+pub type UserAgentParserResult<'a> = woothee::parser::WootheeResult<'a>;
+
+#[derive(Serialize)]
+pub struct UserAgent<'a> {
+    pub name: &'a str,
+    pub category: &'a str,
+    pub os: &'a str,
+    pub os_version: String,
+    pub browser_type: &'a str,
+    pub version: String,
+    pub vendor: &'a str,
+}
+
+impl<'a> From<UserAgentParserResult<'a>> for UserAgent<'a> {
+    fn from(ua: UserAgentParserResult<'a>) -> Self {
+        UserAgent {
+            name: ua.name,
+            category: ua.category,
+            os: ua.os,
+            os_version: ua.os_version,
+            browser_type: ua.browser_type,
+            version: ua.version,
+            vendor: ua.vendor,
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct Ip<'a> {
+    addr: String,
+    version: &'a str,
+}
+
+#[derive(Serialize)]
+pub struct Ifconfig<'a> {
+    ip: Ip<'a>,
+    user_agent: Option<UserAgent<'a>>,
+}
+
+pub fn get_ifconfig<'a>(remote: &'a SocketAddr, user_agent: &Option<&'a str>, user_agent_parser: &'a UserAgentParser) -> Ifconfig<'a> {
+    let ip_addr = format!("{}", remote.ip());
+    let ip_version = if remote.is_ipv4() { "4" } else { "6" };
+    let ip = Ip { addr: ip_addr, version: ip_version };
+    let user_agent = user_agent
+        .and_then(|s| user_agent_parser.parse(s))
+        .map(|res| res.into());
+
+    Ifconfig { ip: ip, user_agent: user_agent }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+extern crate ifconfig_rs;
 extern crate rocket;
 extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
 extern crate serde_json;
-extern crate woothee;
 
+use ifconfig_rs::*;
 use rocket::http::Status;
 use rocket_contrib::{Json, Value as JsonValue};
 use rocket::request::{self, FromRequest};
@@ -14,8 +14,7 @@ use rocket::{Request, State, Outcome};
 use rocket_contrib::Template;
 use std::net::SocketAddr;
 
-
-struct RequesterInfo<'a> {
+pub struct RequesterInfo<'a> {
     remote: SocketAddr,
     user_agent: Option<&'a str>,
 }
@@ -36,63 +35,13 @@ impl<'a, 'r> FromRequest<'a, 'r> for RequesterInfo<'a> {
     }
 }
 
-type UserAgentParser = woothee::parser::Parser;
-type UserAgentParserResult<'a> = woothee::parser::WootheeResult<'a>;
-
 fn init_user_agent_parser() -> UserAgentParser { UserAgentParser::new() }
 
-#[derive(Serialize)]
-pub struct UserAgent<'a> {
-    pub name: &'a str,
-    pub category: &'a str,
-    pub os: &'a str,
-    pub os_version: String,
-    pub browser_type: &'a str,
-    pub version: String,
-    pub vendor: &'a str,
-}
-
-impl<'a> From<UserAgentParserResult<'a>> for UserAgent<'a> {
-    fn from(ua: UserAgentParserResult<'a>) -> Self {
-        UserAgent {
-            name: ua.name,
-            category: ua.category,
-            os: ua.os,
-            os_version: ua.os_version,
-            browser_type: ua.browser_type,
-            version: ua.version,
-            vendor: ua.vendor,
-        }
-    }
-}
-
-
-#[derive(Serialize)]
-struct Ip<'a> {
-    addr: String,
-    version: &'a str,
-}
-
-#[derive(Serialize)]
-struct Ifconfig<'a> {
-    ip: Ip<'a>,
-    user_agent: Option<UserAgent<'a>>,
-}
-
-fn get_ifconfig<'a>(req_info: &'a RequesterInfo, user_agent_parser: &'a UserAgentParser) -> Ifconfig<'a> {
-    let ip_addr = format!("{}", req_info.remote.ip());
-    let ip_version = if req_info.remote.is_ipv4() { "4" } else { "6" };
-    let ip = Ip { addr: ip_addr, version: ip_version };
-    let user_agent = req_info.user_agent
-        .and_then(|s| user_agent_parser.parse(&s))
-        .map(|res| res.into());
-
-    Ifconfig { ip: ip, user_agent: user_agent }
-}
-
 #[get("/", format = "application/json", rank=1)]
+#[allow(unknown_lints)] // for clippy
+#[allow(needless_pass_by_value)] // params are passed by value
 fn index_json(req_info: RequesterInfo, user_agent_parser: State<UserAgentParser>) -> Option<Json<JsonValue>> {
-    let ifconfig = get_ifconfig(&req_info, &user_agent_parser);
+    let ifconfig = get_ifconfig(&req_info.remote, &req_info.user_agent, &user_agent_parser);
 
     // 'Json(ifconfig)' requires a lifetime in the return value, which we cannot supply. Therefore, we serialize manually
     match serde_json::to_value(ifconfig){
@@ -102,9 +51,11 @@ fn index_json(req_info: RequesterInfo, user_agent_parser: State<UserAgentParser>
 }
 
 #[get("/", rank=2)]
+#[allow(unknown_lints)] // for clippy
+#[allow(needless_pass_by_value)] // params are passed by value
 fn index_html(req_info: RequesterInfo, user_agent_parser: State<UserAgentParser>) -> Template {
-    let ifconfig = get_ifconfig(&req_info, &user_agent_parser);
-    
+    let ifconfig = get_ifconfig(&req_info.remote, &req_info.user_agent, &user_agent_parser);
+
     Template::render("index", &ifconfig)
 }
 
