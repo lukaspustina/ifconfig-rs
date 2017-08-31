755692da584f455d46a2f46c09a64bd6fe5c385c
Lukas Pustina <lukas@pustina.net>
2017-08-31 10:09:36 +0200

    Add Json response and some refactoring

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@
 extern crate rocket;
 extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
+extern crate serde_json;
 extern crate woothee;
 
 use rocket::http::Status;
+use rocket_contrib::{Json, Value as JsonValue};
 use rocket::request::{self, FromRequest};
 use rocket::{Request, State, Outcome};
 use rocket_contrib::Template;
@@ -72,13 +74,12 @@ struct Ip<'a> {
 }
 
 #[derive(Serialize)]
-struct IndexContext<'a> {
-    ip: &'a Ip<'a>,
+struct Ifconfig<'a> {
+    ip: Ip<'a>,
     user_agent: Option<UserAgent<'a>>,
 }
 
-#[get("/")]
-fn index(req_info: RequesterInfo, user_agent_parser: State<UserAgentParser>) -> Template {
+fn get_ifconfig<'a>(req_info: &'a RequesterInfo, user_agent_parser: &'a UserAgentParser) -> Ifconfig<'a> {
     let ip_addr = format!("{}", req_info.remote.ip());
     let ip_version = if req_info.remote.is_ipv4() { "4" } else { "6" };
     let ip = Ip { addr: ip_addr, version: ip_version };
@@ -86,13 +87,30 @@ fn index(req_info: RequesterInfo, user_agent_parser: State<UserAgentParser>) -> 
         .and_then(|s| user_agent_parser.parse(&s))
         .map(|res| res.into());
 
-    let context = IndexContext { ip: &ip, user_agent: user_agent };
-    Template::render("index", &context)
+    Ifconfig { ip: ip, user_agent: user_agent }
+}
+
+#[get("/", format = "application/json", rank=1)]
+fn index_json(req_info: RequesterInfo, user_agent_parser: State<UserAgentParser>) -> Option<Json<JsonValue>> {
+    let ifconfig = get_ifconfig(&req_info, &user_agent_parser);
+
+    // 'Json(ifconfig)' requires a lifetime in the return value, which we cannot supply. Therefore, we serialize manually
+    match serde_json::to_value(ifconfig){
+        Ok(json) => Some(Json(json)),
+        Err(_) => None
+    }
+}
+
+#[get("/", rank=2)]
+fn index_html(req_info: RequesterInfo, user_agent_parser: State<UserAgentParser>) -> Template {
+    let ifconfig = get_ifconfig(&req_info, &user_agent_parser);
+    
+    Template::render("index", &ifconfig)
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![index_html, index_json])
         .attach(Template::fairing())
         .manage(init_user_agent_parser())
         .launch();
