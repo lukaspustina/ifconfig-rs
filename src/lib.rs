2938938c2006e77548a8f97f5937c25841f6aa2f
Lukas Pustina <lukas@pustina.net>
2017-09-01 12:56:19 +0200

    Add tcp and user_agent_header str to ifconfig

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,19 @@ use maxminddb::geoip2;
 use std::net::SocketAddr;
 
 #[derive(Serialize)]
+pub struct Host {
+    name: String,
+}
+
+#[derive(Serialize)]
 pub struct Ip<'a> {
     addr: String,
     version: &'a str,
 }
 
 #[derive(Serialize)]
-pub struct Host {
-    name: String,
+pub struct Tcp {
+    port: u16,
 }
 
 pub struct GeoIpCityDb(pub maxminddb::Reader);
@@ -76,17 +81,24 @@ impl<'a> From<UserAgentParserResult<'a>> for UserAgent<'a> {
 
 #[derive(Serialize)]
 pub struct Ifconfig<'a> {
-    ip: Ip<'a>,
     host: Host,
+    ip: Ip<'a>,
+    tcp: Tcp,
     location: Option<Location>,
     isp: Option<Isp>,
     user_agent: Option<UserAgent<'a>>,
+    user_agent_header: Option<&'a str>,
 }
 
-pub fn get_ifconfig<'a>(remote: &'a SocketAddr, user_agent: &Option<&'a str>, user_agent_parser: &'a UserAgentParser, geoip_city_db: &'a GeoIpCityDb, geoip_asn_db: &'a GeoIpAsnDb) -> Ifconfig<'a> {
+pub fn get_ifconfig<'a>(remote: &'a SocketAddr, user_agent_header: &Option<&'a str>, user_agent_parser: &'a UserAgentParser, geoip_city_db: &'a GeoIpCityDb, geoip_asn_db: &'a GeoIpAsnDb) -> Ifconfig<'a> {
+    let hostname = dns_lookup::lookup_addr(&remote.ip()).expect("not found");
+    let host = Host { name: hostname };
+
     let ip_addr = format!("{}", remote.ip());
     let ip_version = if remote.is_ipv4() { "4" } else { "6" };
     let ip = Ip { addr: ip_addr, version: ip_version };
+
+    let tcp = Tcp { port: remote.port() };
 
     let geo_city: Option<geoip2::City> = geoip_city_db.0.lookup(remote.ip()).ok();
     let location = geo_city
@@ -100,12 +112,9 @@ pub fn get_ifconfig<'a>(remote: &'a SocketAddr, user_agent: &Option<&'a str>, us
     let geo_isp: Option<geoip2::Isp> = geoip_asn_db.0.lookup(remote.ip()).ok();
     let isp = geo_isp.map(|isp| Isp { name: isp.autonomous_system_organization });
 
-    let hostname = dns_lookup::lookup_addr(&remote.ip()).expect("not found");
-    let host = Host { name: hostname };
-
-    let user_agent = user_agent
+    let user_agent = user_agent_header
         .and_then(|s| user_agent_parser.parse(s))
         .map(|res| res.into());
 
-    Ifconfig { ip, host, location, isp, user_agent }
+    Ifconfig { host, ip, tcp, location, isp, user_agent, user_agent_header: *user_agent_header }
 }
